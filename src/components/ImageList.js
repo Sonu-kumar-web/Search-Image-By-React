d013700d0563c77228411a53a933f22a6850ce18
Sonu <sonu3660@gmail.com>
2020-08-05 18:57:41 +0530

    Add key amd alt propert inside img tag

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,9 @@ import React from "react";
 const ImageList = (props) => {
    //    console.log(props.images);
    const images = props.images.map((image) => {
-      return <img src={image.urls.regular} />;
+      return (
+         <img key={image.id} alt={image.description} src={image.urls.regular} />
+      );
    });
    return <div>{images}</div>;
 };
