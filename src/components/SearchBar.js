d25b109fd4b275d3e61418d8c31cdda93ad8fe0b
Sonu <sonu3660@gmail.com>
2020-08-06 17:38:39 +0530

    Add some colors

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
 
    render() {
       return (
-         <div className="ui segment">
+         <div className="ui segment" style={{ backgroundColor: "linen" }}>
             <form onSubmit={this.onFormSubmit} className="ui form">
                <div className="field">
                   <label>Image Search</label>
